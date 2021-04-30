4bee34a3f0a516bb256041b57f6b0d2412b63574
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2021-04-30 03:07:40 -0300

    Few changes

--- Project39/PlayData.swift
+++ Project39/PlayData.swift
@@ -9,22 +9,29 @@ import Foundation
 
 class PlayData {
 	var allWords = [String]()
-	var wordCounts = [String: Int]()
+	var filteredWords = [String]()
+	var wordCounts: NSCountedSet!
 	
 	init() {
 		if let path = Bundle.main.path(forResource: "plays", ofType: "txt") {
 			if let plays = try? String(contentsOfFile: path) {
 				allWords = plays.components(separatedBy: CharacterSet.alphanumerics.inverted)
 				allWords = allWords.filter { $0 != "" }
-				print("Word count: \(allWords.count)")
 				
-				for word in allWords {
-					wordCounts[word, default: 0] += 1
-				}
-				
-				allWords = Array(wordCounts.keys)
-				
+				wordCounts = NSCountedSet(array: allWords)
+				let sorted = wordCounts.allObjects.sorted { wordCounts.count(for: $0) > wordCounts.count(for: $1) }
+				allWords = sorted as! [String]
 			}
 		}
-	}	
+	}
+	func applyUserFilter(_ input: String) {
+		if let wordCount = Int(input) {
+			filteredWords = allWords.filter { wordCounts.count(for: $0) >= wordCount }
+		} else {
+			filteredWords = allWords.filter { $0.range(of: input, options: .caseInsensitive) != nil }
+		}
+	}
+	func applyFilter(_ filter: (String) -> Bool) {
+		filteredWords = allWords.filter(filter)
+	}
 }
--- Project39Tests/Project39Tests.swift
+++ Project39Tests/Project39Tests.swift
@@ -10,32 +10,55 @@ import XCTest
 
 class Project39Tests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+//    override func setUpWithError() throws {
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    }
+//
+//    override func tearDownWithError() throws {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    }
 	func testAllWordsLoaded() {
 		let playData = PlayData()
 		XCTAssertEqual(playData.allWords.count, 18440, "allWords count was not equal to 18440")
 	}
-	
 	func testWordCountsAreCorrect() {
 		let playData = PlayData()
-		XCTAssertEqual(playData.wordCounts["entering"], 4, "entering does not appear 4 times")
-		XCTAssertEqual(playData.wordCounts["pense"], 1, "pense does not appear 1 time")
-		XCTAssertEqual(playData.wordCounts["Gentleman"], 115, "Gentleman does not appear 115 times")
-		XCTAssertEqual(playData.wordCounts["Shylock"], 19, "Shylock does not appear 19 times")
-		XCTAssertEqual(playData.wordCounts["Are"], 133, "Are does not appear 133 times")
-		XCTAssertEqual(playData.wordCounts["NESTOR"], 50, "NESTOR does not appear 50 times")
+		XCTAssertEqual(playData.wordCounts.count(for: "entering"), 4, "entering does not appear 4 times")
+		XCTAssertEqual(playData.wordCounts.count(for: "pense"), 1, "pense does not appear 1 time")
+		XCTAssertEqual(playData.wordCounts.count(for: "Gentleman"), 115, "Gentleman does not appear 115 times")
+		XCTAssertEqual(playData.wordCounts.count(for: "Shylock"), 19, "Shylock does not appear 19 times")
+		XCTAssertEqual(playData.wordCounts.count(for: "Are"), 133, "Are does not appear 133 times")
+		XCTAssertEqual(playData.wordCounts.count(for: "NESTOR"), 50, "NESTOR does not appear 50 times")
 
 	}
-	
+	func testUserFilterWorks() {
+		let playData = PlayData()
+		
+		playData.applyUserFilter("100")
+		XCTAssertEqual(playData.filteredWords.count, 495)
+		
+		playData.applyUserFilter("1000")
+		XCTAssertEqual(playData.filteredWords.count, 55)
+
+		playData.applyUserFilter("10000")
+		XCTAssertEqual(playData.filteredWords.count, 1)
+
+		playData.applyUserFilter("test")
+		XCTAssertEqual(playData.filteredWords.count, 56)
+
+		playData.applyUserFilter("swift")
+		XCTAssertEqual(playData.filteredWords.count, 7)
+
+		playData.applyUserFilter("objective-c")
+		XCTAssertEqual(playData.filteredWords.count, 0)
+	}
 	func testViewController() {
 		let vc = ViewController()
 		vc.viewDidLoad()
 	}
-	
+	func testWordsLoadSpeed() {
+		measure {
+			_ = PlayData()
+		}
+	}
 }
